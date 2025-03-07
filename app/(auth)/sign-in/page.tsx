eeff005678fa9a8673559530dd9af2670744bc63
Charles Nkonoki <nkonoki.charles@gmail.com>
2025-03-07 10:23:33 +0300

    initialize project structure with initial files and configurations

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { loginAction } from "@/actions/auth.actions"
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+const Login = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+  
+  // 2. Define a submit handler.
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    const formData = new FormData();
+    formData.append("email", values.email);
+    formData.append("password", values.password);
+    console.log(formData)
+    loginAction(formData)
+  }  
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="email" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="Password" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />        
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
+
+export default Login
---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import { updateSession } from '@/utils/supabase/middleware'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function middleware(request: NextRequest) {
+  // Get the hostname and pathname from the request
+  const hostname = request.headers.get('host')
+  const pathname = request.nextUrl.pathname
+
+  // Exclude localhost:3000/ from the authentication check
+  if (hostname === 'localhost:3000' && pathname === '/') {
+    return NextResponse.next() // Allow access without authentication
+  }
+
+  // If not the landing page, proceed with authentication or session update
+  return await updateSession(request)
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - Any image files (svg, png, jpg, jpeg, gif, webp)
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+}
