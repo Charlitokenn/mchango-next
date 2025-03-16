f184eb9c602ef478719f479b6184a891ad74fae3
Charles Nkonoki <nkonoki.charles@gmail.com>
2025-03-16 21:38:14 +0300

    testing

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,6 +1,27 @@
 import AuthForm from "@/components/authForm"
 
 const SignUp = async () => {
+  const sendMessage = async () => {
+    try {
+        const response = await fetch('/api/sendMessage', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                to: '+255745896952',
+                message: 'Hello World!!',
+            })
+        });
+
+        const result = await response.json();
+        console.log('Response:', result);
+    } catch (error) {
+        console.error('Error sending message:', error);
+    }
+};
+
+
+sendMessage()
+
   return (
     <section className="flex-center size-full max-sm:px-6">
       <AuthForm type="sign-in" />
